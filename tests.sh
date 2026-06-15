e9c9a24c447fa658bd7f67126dcf5f22ffaeeb53
4rthur-Root <kpodonougael@gmail.com>
2026-06-15 09:06:03 +0000

    Adding tests file to the project

---
+++ tests.sh
@@ -0,0 +1,16 @@
+echo "== Test des installations =="
+ansible --version
+terraform --version
+
+echo "== Test socket podman =="
+echo $XDG_RUNTIME_DIR  # Doit retourner /run/user/1000 ou similaire
+ls $XDG_RUNTIME_DIR/podman/podman.sock
+
+echo "== Test Podman + image Juice Shop =="
+podman run -d --name test-juiceshop -p 3000:3000 bkimminich/juice-shop:latest
+
+sleep 10
+
+curl localhost:3000 | grep "OWASP Juice Shop"
+
+podman rm -f test-juiceshop  # Nettoyage après test
